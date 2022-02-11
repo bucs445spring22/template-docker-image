d5d943b22cc7208bfe70a06464f66c1b473c6841
Steven <stevenallenmoore@gmail.com>
2022-02-11 16:14:35 -0500

    Updated, working.

--- app/main.py
+++ app/main.py
@@ -1,23 +1,40 @@
-import requests
+import json
 import random
+
+import requests
 
 def main():
     result = requests.get("https://opentdb.com/api.php?amount=3")
-    score = 0
     data = result.json()
+
+    score = 0
+    
     for k in data['results']:
+
         print(f'{k["question"]}')
+
         answers = k['incorrect_answers']+[k['correct_answer']]
+
         random.shuffle(answers)
+        
         [print(f"\t{i}) {a}") for i, a in enumerate(answers)]
+
         guess = int(input("Which answer do you think is correct?"))
+
         if answers[guess] == k['correct_answer']:
             score += 1
+            print("Correct!")
+        else:
+            print(f"nope: {k['correct_answer']}")
+
         input("[press any key to continue]")
+
+    print(f"You got {score} right!")
 
     if input("Would you like to save your score? [Y/n]").lower() == 'y':
         name = input("What is your name?")
-        result = requests.post("http://db:8000/save", data={name: score})
+        headers = {'Content-Type': 'application/json'}
+        result = requests.post("http://db:8000/save", data=json.dumps({name: score}), headers=headers)
         print(result)
 
     result = requests.get("http://db:8000/all")
